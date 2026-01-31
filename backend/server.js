0d0102959f583b5331ac0fcb5a6e6405ddd77024
ilonahg <gogilchyn2005ilona@gmail.com>
2026-02-01 00:31:31 +0200

    fix cors

--- backend/server.js
+++ backend/server.js
@@ -28,11 +28,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: [
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-        "https://69e735d72d00e189c786171--chimerical-kitsune-11c58a.netlify.app"
-    ],
+    origin: true,
     credentials: true
 }));
 
