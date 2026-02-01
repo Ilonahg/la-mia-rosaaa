d91c1b64873c1376070a447874699fda04d0308a
ilonahg <gogilchyn2005ilona@gmail.com>
2026-02-01 20:30:50 +0200

    auth fix

--- main.js
+++ main.js
@@ -773,7 +773,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const authCode = document.getElementById("authCode");
 
     const authEmail = document.getElementById("authEmail");
-    const authSubmit = document.querySelector("#authOverlay button, .login-modal button, .btn");
+    const authSubmit = document.getElementById("authSubmitBtn");
+
 
     const authMessage = document.getElementById("authMessage");
 
